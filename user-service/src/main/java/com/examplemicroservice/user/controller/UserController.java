b8db634eb6aa68a0a958fb93fccac3564b196f65
Saif <saifali.shaikh2509@gmail.com>
2023-08-28 12:11:37 +0530

    Added Circuit breaker resilience4j

--- user-service/src/main/java/com/examplemicroservice/user/controller/UserController.java
+++ user-service/src/main/java/com/examplemicroservice/user/controller/UserController.java
@@ -8,9 +8,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.examplemicroservice.user.VO.Department;
 import com.examplemicroservice.user.VO.ResponseTemplateVO;
 import com.examplemicroservice.user.entity.Users;
 import com.examplemicroservice.user.service.UserService;
+
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 
 @RestController
 @RequestMapping("/users")
@@ -18,6 +21,8 @@ public class UserController {
 
 	@Autowired
 	private UserService userService;
+	
+	public static final String USER_SERVICE = "userService";
 	
 	@PostMapping("/")
 	public Users saveUser(@RequestBody Users user) {
@@ -29,10 +34,37 @@ public class UserController {
 	 * userId) { return userService.findUserById(userId); }
 	 */
 	
+	/*
+	 * @GetMapping("/{id}") public ResponseTemplateVO
+	 * getUserWithDepartment(@PathVariable("id") Long userId) { return
+	 * userService.getUserWithDepartment(userId); }
+	 */
+	
 	@GetMapping("/{id}")
+	@CircuitBreaker(name = USER_SERVICE, fallbackMethod = "userServiceFallBackMethod")
 	public ResponseTemplateVO getUserWithDepartment(@PathVariable("id") Long userId) {
 		return userService.getUserWithDepartment(userId);
 	}
 	
+	public ResponseTemplateVO userServiceFallBackMethod(Exception exp) {
+		ResponseTemplateVO vo = new ResponseTemplateVO();
+		
+		Department dept = new Department();
+		dept.setDepartmentAddress("Sample Address");
+		dept.setDepartmentCode("Sample code");
+		dept.setDepartmentId((long) 1001);
+		dept.setDepartmentName("Sample name");
+		
+		vo.setDepartment(dept);
+		
+		Users user = new Users();
+		user.setDepartmentId((long) 10001);
+		user.setEmail("sample email");
+		user.setFirstName("sample first name");
+		user.setLastName("Sample last name");
+		user.setUserId((long) 10020);
+		vo.setUser(user);
+		return vo;
+	}
 	
 }
